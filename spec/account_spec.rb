6c3af0c656489918e251e7dbb1da6e1fa68b3442
jdm79 <james.malvern@gmail.com>
2018-10-08 16:16:54 +0100

    fix: changes Account to described_class, deletes unnecessary whitespace

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,67 +1,61 @@
 require 'account'
 
-RSpec.describe Account do 
+RSpec.describe Account do
+  describe 'balance' do
+    subject { described_class.new }
 
-  context "balance" do
-
-    subject { Account.new }
-
-    it "should create a new account" do 
-      expect(subject).to be_instance_of(Account)
+    it 'should create a new account' do 
+      expect(subject).to be_instance_of(described_class)
     end
 
-    it "should start with an initial balance of 0 if no credit initially deposited" do
-      expect(subject.get_balance).to eq(0)
+    it 'should start with an initial balance of 0 if no credit initially deposited' do
+      expect(subject.balance).to eq(0)
     end
 
-    it "should calculate a series of deposits and withdrawals" do
+    it 'should calculate a series of deposits and withdrawals' do
       subject.deposit_amount(1000)
       subject.deposit_amount(2000)
       subject.withdraw_amount(500)
-      expect(subject.get_balance).to eq(2500)
+      expect(subject.balance).to eq(2500)
     end
 
   end
 
-  context 'initial deposit' do
+  describe 'initial deposit' do
+    subject { described_class.new(100) }
 
-    subject { Account.new(100) }
-
-    it "should start with an initial balance of 100 if 100 initially deposited" do
-      expect(subject.get_balance).to eq(100)
+    it 'should start with an initial balance of 100 if 100 initially deposited' do
+      expect(subject.balance).to eq(100)
     end
 
   end
 
-  context 'deposit' do
+  describe 'deposit' do
+    subject { described_class.new }
 
-    subject { Account.new }
-
-    it "should increment the balance accordingly" do
-      expect{subject.deposit_amount(50)}.to change{subject.get_balance}.by(50)
+    it 'should increment the balance accordingly' do
+      expect{subject.deposit_amount(50)}.to change{subject.balance}.by(50)
     end
 
   end
 
-  context 'withdrawal' do
+  describe 'withdrawal' do
+    subject { described_class.new(100) }
 
-    subject { Account.new(100) }
-
-    it "should return a balance minus the withdrawal" do
-      expect{subject.withdraw_amount(50)}.to change{subject.get_balance}.by(-50)
+    it 'should return a balance minus the withdrawal' do
+      expect{subject.withdraw_amount(50)}.to change{subject.balance}.by(-50)
     end
 
   end
 
-  context 'print statement' do 
+  describe 'print statement' do 
+    subject { described_class.new }
 
-    subject { Account.new }
-
-    it "should print a log of statements" do
+    it 'should print a log of statements' do
       subject.deposit_amount(1000)
       subject.deposit_amount(2000)
       subject.withdraw_amount(500)
-      expect(subject.print_balance).to eq("Your current balance is #{subject.get_balance}")
+      expect(subject.print_balance).to eq("Your current balance is #{subject.balance}")
     end
 
   end
