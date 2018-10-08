ed296eb4ae348dfafdae6423102ec5d925e3a130
jdm79 <james.malvern@gmail.com>
2018-10-08 13:05:36 +0100

    test: adds test to check if user can initialise an account with a given amount passed as an argument

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -10,14 +10,18 @@ RSpec.describe Account do
     end
 
     it "should start with an initial balance of 0 if no credit initially deposited" do
-      
       expect(subject.get_balance).to eq(0)
     end
 
-    # it "should start with an initial balance of 100 if 100 initially deposited" do
-    #   account = Account.new(100)
-    #   expect(account.get_balance).to eq(100)
-    # end
+  end
+
+  context 'deposit' do
+
+    subject { Account.new(100) }
+
+    it "should start with an initial balance of 100 if 100 initially deposited" do
+      expect(subject.get_balance).to eq(100)
+    end
 
   end
 
