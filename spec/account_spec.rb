f519b3df7a6bf2cc6abc0b0d6eff2c1a55224a03
jdm79 <james.malvern@gmail.com>
2018-10-08 13:00:10 +0100

    test: adds new test to check user can create a new instance of Account, also using 'subject' as the obj to test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,16 +3,21 @@ require 'account'
 RSpec.describe Account do 
 
   context "balance" do
+    subject { Account.new }
+
+    it "should create a new account" do 
+      expect(subject).to be_instance_of(Account)
+    end
 
     it "should start with an initial balance of 0 if no credit initially deposited" do
-      account = Account.new(0)
-      expect(account.get_balance).to eq(0)
+      
+      expect(subject.get_balance).to eq(0)
     end
 
-    it "should start with an initial balance of 100 if 100 initially deposited" do
-      account = Account.new(100)
-      expect(account.get_balance).to eq(100)
-    end
+    # it "should start with an initial balance of 100 if 100 initially deposited" do
+    #   account = Account.new(100)
+    #   expect(account.get_balance).to eq(100)
+    # end
 
   end
 
