6a78937f8209930829e9eb066d8984fcf84b3090
jdm79 <james.malvern@gmail.com>
2018-10-09 16:16:06 +0100

    refactor: adds an update_history method using the record method from TransactionHistory class

--- lib/account.rb
+++ lib/account.rb
@@ -1,25 +1,35 @@
 require_relative 'statement_printer'
+require_relative 'transaction'
+require_relative 'transaction_history'
+require_relative 'date'
 # Main class
 class Account
   attr_reader :balance, :transactions
   def initialize(balance = 0)
     @balance = balance
-    @transactions = []
+    @transactions = TransactionHistory.new
   end
 
   def deposit_amount(amount, date = Date.new.current_date)
     @balance += amount
     details = { date: date, credit: amount, debit: 0, balance: @balance }
-    transactions.push(details)
+    update_history(Transaction.new(details))
   end
 
   def withdraw_amount(amount, date = Date.new.current_date)
     @balance -= amount
     details = { date: date, credit: 0, debit: amount, balance: @balance }
-    transactions.push(amount)
+    update_history(Transaction.new(details))
   end
 
-  def print_statement()
+  def print_statement
     statement = StatementPrinter.new(@transactions).print
   end
+
+  private
+
+  def update_history(transaction)
+    @transactions.record(transaction)
+  end
+
 end
