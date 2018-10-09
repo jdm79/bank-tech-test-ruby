782823bf9ab700374e91235baa36e392d84a22f0
jdm79 <james.malvern@gmail.com>
2018-10-09 12:37:38 +0100

    test: get rid of trying to test the style output of #print

--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -6,11 +6,5 @@ RSpec.describe StatementPrinter do
     it 'creates a new statement printer' do
       expect(st).to be_instance_of(described_class)
     end
-
-
-    it 'prints a statement' do
-      st = StatementPrinter.new(50)
-      expect(st.print).to eq('Current balance: 50')
-    end
   end
 end
