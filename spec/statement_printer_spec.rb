820e8179e6091dd14b74cffa0902655eb4023bd1
jdm79 <james.malvern@gmail.com>
2018-10-09 10:52:06 +0100

    test: adds test for the #print method

--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -7,5 +7,9 @@ RSpec.describe StatementPrinter do
     it 'creates a new statement printer' do
       expect(subject).to be_instance_of(described_class)
     end
+
+    it 'prints a statement' do
+      expect(subject.print).to eq('hello world')
+    end
   end
 end
