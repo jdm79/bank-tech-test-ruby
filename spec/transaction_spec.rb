7fd5ef154ce1c75db83ed01a39becf6e075b3ed5
jdm79 <james.malvern@gmail.com>
2018-10-09 15:14:45 +0100

    test: adds tests for transaction details

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,11 +1,24 @@
 require "transaction"
 
 describe Transaction do
-  txn = {date: "01/01/2012", credit: 100, debit: 0, balance: 100}
+  txn = {date: "09/09/2018", credit: 100, debit: 50, balance: 50}
   subject { Transaction.new(txn) }
 
   it "creates a new transaction" do
     expect(subject).to be_instance_of(Transaction)
+  end
+
+  it "has a date value of 09/09/2018" do
+    expect(subject.date).to eq("09/09/2018")
+  end
+  it "has a credit value of 100" do
+    expect(subject.credit).to eq(100)
+  end
+  it "has a debit value of 50" do
+    expect(subject.debit).to eq(50)
+  end
+  it "has a balance value of 50" do
+    expect(subject.balance).to eq(50)
   end
 end
 
